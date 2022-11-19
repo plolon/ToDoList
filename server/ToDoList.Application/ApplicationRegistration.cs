9c5a7a05428ce27c6245e1586cae3e1a568c14b3
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2022-11-19 23:45:23 +0100

    Added controller and configured mediatr

---
+++ server/ToDoList.Application/ApplicationRegistration.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ToDoList.Application
+{
+    public static class ApplicationRegistration
+    {
+        public static IServiceCollection ConfigureApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
