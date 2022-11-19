6f3faec6c8069a506574792fa65c13d15de47504
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2022-11-19 23:31:35 +0100

    Added queries for workitem

--- server/ToDoList.Infrastructure/ToDoListDBContext.cs
+++ server/ToDoList.Infrastructure/ToDoListDBContext.cs
@@ -3,12 +3,26 @@ using ToDoList.Domain.Models;
 
 namespace ToDoList.Infrastructure
 {
-    public class ToDoListDBContext :DbContext
+    public class ToDoListDBContext : DbContext
     {
         public ToDoListDBContext(DbContextOptions<ToDoListDBContext> options) : base(options)
         {
         }
-        
+
         public DbSet<WorkItem> WorkItems { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                if (entry.State.Equals(EntityState.Added))
+                {
+                    entry.Entity.CreatedOn = DateTime.Now;
+                }
+                entry.Entity.ModifiedOn = DateTime.Now;
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
