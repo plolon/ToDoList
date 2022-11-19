c3c720e7fcf67418a78bb8db9fce0d93fc9fcce8
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2022-11-19 21:24:08 +0100

    Added infrastructure registration

---
+++ server/ToDoList.Infrastructure/InfrastructureRegistration.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ToDoList.Domain.Repositories;
+using ToDoList.Infrastructure.Repositories;
+
+namespace ToDoList.Infrastructure
+{
+    public static class InfrastructureRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ToDoListDBContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            
+            return services;
+        }
+    }
+}
